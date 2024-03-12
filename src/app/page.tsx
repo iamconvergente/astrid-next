6c5b7fb4eb95fc4dd5926388f7b1ffa169687c60
Convergente Studio <convergentepro@gmail.com>
2024-03-12 18:32:50 +0000

    changes on styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import type { Metadata } from "next";
+
+export const meta: Metadata = {
+  title: "Astrid Torrente Official Website | ASTAAMVI",
+  description: "",
+};
 export default function Home() {
-	return (
-		<>
-			<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-				<h1>Home Page Here</h1>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Home Page Here</h1>
+      </main>
+    </>
+  );
 }
